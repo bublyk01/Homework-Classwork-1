450dd7fc0ab634866ea4e7a6cdf87a6c85be6dfd
Volodya Bublyk <151150647+bublyk01@users.noreply.github.com>
2024-07-08 12:11:55 +0300

    Task 4

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include <vector>
+#include <memory>
 
 class animal {
 public:
 	virtual void say() const = 0;
+	virtual ~animal() = default;
 };
 
 class dog : public animal {
@@ -20,11 +23,14 @@ public:
 };
 
 int main() {
-	dog Dog;
-	cat Cat;
+	std::vector<std::unique_ptr<animal>> animals;
 
-	Dog.say();
-	Cat.say();
+	animals.push_back(std::make_unique<dog>());
+	animals.push_back(std::make_unique<cat>());
+
+	for (const auto& animal : animals) {
+		animal->say();
+	}
 
 	return 0;
 }
